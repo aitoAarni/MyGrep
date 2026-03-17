210e5a2b7566328e7e347575046b4a52f838a61f
aitoAarni <aarni.rechardt@gmail.com>
2026-03-17 21:35:03 +0200

    moove << operator

--- src/search_file.cpp
+++ src/search_file.cpp
@@ -9,6 +9,19 @@
 
 namespace fs = std::filesystem;
 
+inline std::ostream& operator<<(std::ostream& os, LineMatch& line_match) {
+    std::string_view line_sw (line_match.line);
+    int current_pos {0};
+    for (const int& match_pos : line_match.match_starts) {
+        os << line_sw.substr(current_pos, match_pos - current_pos);
+        os << "\033[1m\033[34m";
+        os << line_sw.substr(match_pos, line_match.match_size);
+        os << "\033[0m";
+        current_pos = match_pos + line_match.match_size;
+    }
+    os << line_sw.substr(current_pos, line_sw.size() - current_pos);
+    return os;
+}
 
 std::vector<FileMatch> search_files(const std::string& path_string, const std::string_view match) {
 
